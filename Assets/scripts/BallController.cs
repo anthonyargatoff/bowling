caf80b1092ecae1055242c4ce344ee6ee5b804de
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-14 17:29:19 -0800

    Add reset

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -6,16 +6,26 @@ public class BallController : MonoBehaviour
   [SerializeField] private Transform ballAnchor;
   [SerializeField] private InputManager inputManager;
   [SerializeField] private Transform launchIndicator;
+  [SerializeField] private AudioSource strikeSound;
   private Rigidbody ballRigidbody;
-  private bool isBallLaunched = false;
+  private bool isBallLaunched;
+  private bool hitPin;
 
   void Start()
   {
     ballRigidbody = GetComponent<Rigidbody>();
     inputManager.OnSpacePressed.AddListener(LaunchBall);
+    ResetBall();
+  }
+
+  public void ResetBall()
+  {
+    isBallLaunched = false;
+    ballRigidbody.isKinematic = true;
+    launchIndicator.gameObject.SetActive(true);
     transform.parent = ballAnchor;
     transform.localPosition = Vector3.zero;
-    ballRigidbody.isKinematic = true;
+    hitPin = false;
   }
 
   void Update()
@@ -31,5 +41,14 @@ public class BallController : MonoBehaviour
     ballRigidbody.isKinematic = false;
     ballRigidbody.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
     launchIndicator.gameObject.SetActive(false);
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Pin" && !hitPin)
+    {
+      hitPin = true;
+      strikeSound.Play();
+    }
   }
 }
